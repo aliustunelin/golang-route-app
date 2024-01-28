df384a87042ca89f9f53af2c2172a50889394843
unknown <aliustunelin@gmail.com>
2024-01-28 06:28:08 +0300

    adding rateLimit

--- go.mod
+++ go.mod
@@ -3,7 +3,6 @@ module golang-route-app
 go 1.21.6
 
 require (
-	github.com/gofiber/fiber v1.14.6
 	github.com/gofiber/fiber/v2 v2.52.0
 	github.com/golang/mock v1.6.0
 	github.com/joho/godotenv v1.5.1
@@ -15,17 +14,17 @@ require (
 	github.com/andybalholm/brotli v1.1.0 // indirect
 	github.com/davecgh/go-spew v1.1.1 // indirect
 	github.com/go-stack/stack v1.8.0 // indirect
-	github.com/gofiber/utils v0.0.10 // indirect
 	github.com/golang/snappy v0.0.1 // indirect
 	github.com/google/uuid v1.6.0 // indirect
-	github.com/gorilla/schema v1.1.0 // indirect
-	github.com/klauspost/compress v1.17.4 // indirect
+	github.com/klauspost/compress v1.17.5 // indirect
 	github.com/mattn/go-colorable v0.1.13 // indirect
 	github.com/mattn/go-isatty v0.0.20 // indirect
 	github.com/mattn/go-runewidth v0.0.15 // indirect
+	github.com/philhofer/fwd v1.1.2 // indirect
 	github.com/pkg/errors v0.9.1 // indirect
 	github.com/pmezard/go-difflib v1.0.0 // indirect
-	github.com/rivo/uniseg v0.4.4 // indirect
+	github.com/rivo/uniseg v0.4.6 // indirect
+	github.com/tinylib/msgp v1.1.8 // indirect
 	github.com/valyala/bytebufferpool v1.0.0 // indirect
 	github.com/valyala/fasthttp v1.51.0 // indirect
 	github.com/valyala/tcplisten v1.0.0 // indirect
--- main.go
+++ main.go
@@ -5,8 +5,10 @@ import (
 	"golang-route-app/configs"
 	"golang-route-app/repos"
 	"golang-route-app/services"
+	"time"
 
 	"github.com/gofiber/fiber/v2"
+	"github.com/gofiber/fiber/v2/middleware/limiter"
 )
 
 // projeyi mainden ayağa kaldıraz
@@ -22,13 +24,58 @@ func main() {
 
 	td := app.LocationHandler{Service: services.NewLocationService(TodoRepoDb)}
 
-	appRoute.Post("/api/location", td.CreateLocation)
-	appRoute.Get("/api/locations", td.GetAllLocation)
+	limiterConfig := limiter.Config{
+		Max:        20,
+		Expiration: time.Second * 30,
+		KeyGenerator: func(c *fiber.Ctx) string {
+			return c.IP()
+		},
+	}
+	appRoute.Use(limiter.New(limiterConfig))
+
+	appRoute.Post("/api/createLocation", func(c *fiber.Ctx) error {
+		if c.Locals("limit") != nil {
+			return c.Status(fiber.StatusTooManyRequests).JSON(fiber.Map{"error": "Rate limit exceeded"})
+		}
+		return td.CreateLocation(c)
+	})
+
+	appRoute.Get("/api/getLocations", func(c *fiber.Ctx) error {
+
+		if c.Locals("limit") != nil {
+			return c.Status(fiber.StatusTooManyRequests).JSON(fiber.Map{"error": "Rate limit exceeded"})
+		}
+		return td.GetAllLocation(c)
+	})
+
 	//golang'deki id mongo' id değil
-	appRoute.Delete("/api/location/:id", td.DeleteLocation)
-	appRoute.Post("/api/location/:id", td.GetByNameWithDataLocation)
-	appRoute.Post("/api/locaation", td.UpdateByIDLocation)
-	appRoute.Post("/api/routing", td.RoutingLocation)
+	appRoute.Delete("/api/deleteLocation/:id", func(c *fiber.Ctx) error {
+		if c.Locals("limit") != nil {
+			return c.Status(fiber.StatusTooManyRequests).JSON(fiber.Map{"error": "Rate limit exceeded"})
+		}
+		return td.DeleteLocation(c)
+	})
+
+	appRoute.Get("/api/location/:id", func(c *fiber.Ctx) error {
+		if c.Locals("limit") != nil {
+			return c.Status(fiber.StatusTooManyRequests).JSON(fiber.Map{"error": "Rate limit exceeded"})
+		}
+		return td.GetByNameWithDataLocation(c)
+	})
+
+	appRoute.Post("/api/updateLocation", func(c *fiber.Ctx) error {
+		if c.Locals("limit") != nil {
+			return c.Status(fiber.StatusTooManyRequests).JSON(fiber.Map{"error": "Rate limit exceeded"})
+		}
+		return td.UpdateByIDLocation(c)
+	})
+
+	appRoute.Post("/api/routing", func(c *fiber.Ctx) error {
+		if c.Locals("limit") != nil {
+			return c.Status(fiber.StatusTooManyRequests).JSON(fiber.Map{"error": "Rate limit exceeded"})
+		}
+		return td.RoutingLocation(c)
+	})
 	appRoute.Listen(":8080")
 
 }
